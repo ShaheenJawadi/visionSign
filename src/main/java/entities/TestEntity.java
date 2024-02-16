ababb0c99d0ec783f2b9b35f47dd967a1b500636
Amal3111 <amalbouslimi51@gmail.com>
2024-02-16 21:16:42 +0100

    test!!!!

--- src/main/java/entities/TestEntity.java
+++ src/main/java/entities/TestEntity.java
@@ -5,4 +5,29 @@ public class TestEntity {
 
     private String name ;
     private  int test ;
+    private  int test2 ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTest() {
+        return test;
+    }
+
+    public void setTest(int test) {
+        this.test = test;
+    }
+
+    public int getTest2() {
+        return test2;
+    }
+
+    public void setTest2(int test2) {
+        this.test2 = test2;
+    }
 }
