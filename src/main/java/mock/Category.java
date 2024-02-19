420ced8249de176dbd400271fa81c4a39d92454a
shaheenj <jawadishaheen@gmail.com>
2024-02-19 04:20:43 +0100

    Create mock models  -category   -SubCategory    -level

---
+++ src/main/java/mock/Category.java
@@ -0,0 +1,27 @@
+package mock;
+
+public class Category {
+    int id ;
+    String nom  ;
+
+    public Category(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
---
+++ src/main/java/mock/Level.java
@@ -0,0 +1,27 @@
+package mock;
+
+public class Level {
+    int id ;
+    int level ;
+
+    public Level(int id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
