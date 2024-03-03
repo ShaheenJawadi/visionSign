86e40bb156cd4813491cc48142bd0ab39c726208
M-Lyna <lyna.mbarky@esprit.tn>
2024-03-03 23:56:55 +0100

    Add services for both

---
+++ src/main/java/services/Categorie/ICategorieService.java
@@ -0,0 +1,22 @@
+package services.Categorie;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ICategorieService<T> {
+    void addCategorie(T t) throws SQLException;
+
+    void updateCategorie(T t) throws SQLException;
+
+    void deleteCategorie(int id) throws SQLException;
+
+    T getCategorieById(int id) throws SQLException;
+
+    List<T> searchLikeNameCategory(String name) throws SQLException;
+
+    T updateCategoryImage(String image) throws SQLException;
+
+    List<T> sortCategoryListByAttribute(String attribute) throws SQLException;
+
+
+}
